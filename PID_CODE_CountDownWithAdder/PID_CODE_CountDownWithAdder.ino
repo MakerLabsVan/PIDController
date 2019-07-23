5bb9d837e603e2ddbd1b03f33a518ece0ae02e07
Remyzh <51066223+Remyzh@users.noreply.github.com>
2019-07-23 10:23:09 -0700

    Update PID_CODE_CountDownWithAdder.ino

--- PID_CODE_CountDownWithAdder/PID_CODE_CountDownWithAdder.ino
+++ PID_CODE_CountDownWithAdder/PID_CODE_CountDownWithAdder.ino
@@ -1,3 +1,18 @@
+/* Parts: buttons (x3), Relays (x3), Temp Sensor(x1), LCD(x1), potentiometer(x1)
+ * 
+ * Description: 
+ * Upon startup, the LCD will display the temperature and the countdown time(00:00). If OnButton is pushed once, the AddTimeButton 
+ * will be enabled for input and if the temperature is also between 0 – 259F, then solenoid A will turn on. If the temperature is either lower than 0F or higher than 
+ * 259F, then AddTimeButton will be enabled but solenoid A will not turn on. If OnButton is pushed for a second time, then AddTimeButton will be disabled and 
+ * solenoid A, B and C will all turn off. Therefore, every time the onButton is pushed, the opposite state will occur (either from OFF to ON or ON to OFF). Additionally, 
+ * if AddTimeButton is pushed before OnButton, then nothing will happen.
+ * 
+ * If AddTimeButton is pushed once, then 15 minutes is added to the countdown timer and if temperature is also between 0-250F, then solenoids B and C will turn on. 
+ * Each additional push to AddTimeButton will add 15 minutes to the countdown timer but will have no effect on solenoids B and C. If the temperature reaches 250F, 
+ * the countdown timer begins counting down. When the countdown timer reaches zero, solenoids A, B and C all turn off.
+ */
+
+
 #include <LiquidCrystal.h> // includes the LiquidCrystal Library 
 
 #define relay1 7
@@ -6,13 +21,14 @@
 #define relay4 4
 //Note: https://seeeddoc.github.io/Relay_Shield_V2.0/ , Pins 4-7 are taken by relay function and cannot be overloaded by other pins. 
 
+
 #define MAX_TEMP  250
 
-LiquidCrystal lcd(11, 10, 9, 8, 3, 2); // Creates an LC object. Parameters: (rs, enable, d4, d5, d6, d7)
+LiquidCrystal lcd(8, 9, 19, 18, 17, 16); // Creates an LC object. Parameters: (rs, enable, d4, d5, d6, d7)
 
 //Pins:
-int buttonPin = 13;   //pin for button 1 (Digital)
-int buttonPin2 = A2;   //pin for button 2 (Analog)
+int buttonPin = 11;   //pin for button 1 (Digital)
+int buttonPin2 = 13;   //pin for button 2 (Analog)
 
 //Time variables:
 int period = 1000;
@@ -55,8 +71,8 @@ void setup() {
   lcd.clear();
   
   pinMode(buttonPin, INPUT);
-  pinMode(buttonPin2, OUTPUT);// analog pin, switch
-  digitalWrite(buttonPin2, HIGH); //countdown button
+  pinMode(buttonPin2, INPUT);// analog pin, switch
+ 
   
 
 }
@@ -92,7 +108,7 @@ void loop() {
     if (reading2 != buttonState2){
       buttonState2 = reading2;
       
-      if ((reading2 == LOW)){    //not sure why but when analog button is pushed, it goes from HIGH to LOW
+      if ((reading2 == HIGH)){    //not sure why but when analog button is pushed, it goes from HIGH to LOW
         digitalWrite(relay2, HIGH);
         digitalWrite(relay3, HIGH);
       
